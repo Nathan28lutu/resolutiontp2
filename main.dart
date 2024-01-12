c48dc5969e8f43b01d6f67beac8ec45da921ffad
Nathan28lutu <nathanlutu99@gmail.com>
2024-01-12 14:06:34 +0200

    commit

--- main.dart
+++ main.dart
@@ -1,48 +1,10 @@
-void main() {
-  int age = 18;
-  String genre = 'M';
-  String nom = 'NGABO';
-  String postnom = 'LUTU';
-
-  if (age >= 18) {
-    if (genre == 'M') {
-      print('Bonjour monsieur' +
-          '  ' +
-          nom +
-          '  ' +
-          postnom +
-          '  ' +
-          'Vous êtes un majeur');
-    } else if (genre == 'F') {
-      print('Bonjour madame' +
-          '  ' +
-          nom +
-          '  ' +
-          postnom +
-          '  ' +
-          'Vous êtes une majeure');
-    } else {
-      print('Erreur');
-    }
-  } else if (age < 18) {
-    if (genre == 'M') {
-      print('Bonjour monsieur' +
-          '  ' +
-          nom +
-          '  ' +
-          postnom +
-          '  ' +
-          'Vous êtes un mineur');
-    } else if (genre == 'F') {
-      print('Bonjour madame' +
-          '  ' +
-          nom +
-          '  ' +
-          postnom +
-          '  ' +
-          'Vous êtes une mineure');
-    } else {
-      print('Erreur');
-    }
+void afficherBonjourLeMonde(int nombre) {
+  for (int i = 0; i < nombre; i++) {
+    print("BONJOUR LE MONDE");
   }
+}
+
+void main() {
+  int nombre = 5;
+  afficherBonjourLeMonde(nombre);
 }
